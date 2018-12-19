435dc27d8bb947ed9522add35fc9c62d94c7a916
disik <disik69@rambler.ru>
2018-12-19 15:39:15 +0200

    add authentication configs

---
+++ src/main/java/ua/pp/disik/englishroulette/backend/config/AuthenticationProvider.java
@@ -0,0 +1,18 @@
+package ua.pp.disik.englishroulette.backend.config;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class AuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+
+    }
+
+    @Override
+    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+        return null;
+    }
+}
