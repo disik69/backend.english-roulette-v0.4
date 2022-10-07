b814702e6c46634e0b339eb657786866353d4e75
disik <disik69@rambler.ru>
2022-10-07 09:00:29 +0300

    master: set db config

--- src/main/java/ua/pp/disik/englishroulette/backend/controller/WordController.java
+++ src/main/java/ua/pp/disik/englishroulette/backend/controller/WordController.java
@@ -3,15 +3,30 @@ package ua.pp.disik.englishroulette.backend.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import org.springframework.web.bind.annotation.*;
+import ua.pp.disik.englishroulette.backend.entity.Word;
+import ua.pp.disik.englishroulette.backend.service.WordService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Api
 @RestController
 @RequestMapping("/word")
 public class WordController {
+    private final WordService wordService;
+
+    public WordController(WordService wordService) {
+        this.wordService = wordService;
+    }
+
     @GetMapping()
     @ApiImplicitParam(name = "token", value = "token", paramType = "query")
-    String read() {
-        return "read words";
+    List<Word> read() {
+        return StreamSupport.stream(
+                wordService.repository().findAll().spliterator(),
+                false
+        ).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
@@ -22,8 +37,8 @@ public class WordController {
 
     @PostMapping()
     @ApiImplicitParam(name = "token", value = "token", paramType = "query")
-    String create() {
-        return "create word";
+    Word create(@RequestBody Word word) {
+        return wordService.repository().save(word);
     }
 
     @PutMapping("/{id}")
