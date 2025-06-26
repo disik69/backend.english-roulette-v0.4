a9183002c0d9ff25b8e155d9615057b89bb2c81f
disik <disik69@meta.ua>
2025-06-26 18:03:37 +0300

    spring-update: update jwt

--- src/main/java/ua/pp/disik/englishroulette/backend/service/JWTTokenService.java
+++ src/main/java/ua/pp/disik/englishroulette/backend/service/JWTTokenService.java
@@ -1,37 +1,32 @@
 package ua.pp.disik.englishroulette.backend.service;
 
 import io.jsonwebtoken.*;
-import io.jsonwebtoken.impl.compression.GzipCompressionCodec;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ua.pp.disik.englishroulette.backend.entity.JwtToken;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.jsonwebtoken.SignatureAlgorithm.HS256;
-
 @Service
 public class JWTTokenService implements Clock {
-    private static final GzipCompressionCodec COMPRESSION_CODEC = new GzipCompressionCodec();
-
     private final String issuer;
     private final int expirationSec;
     private final int clockSkewSec;
-    private final String secretKey;
+    private final SecretKey secretKey;
 
     JWTTokenService(
             @Value("${jwt.issuer}") String issuer,
             @Value("${jwt.expiration-sec}") int expirationSec,
-            @Value("${jwt.clock-skew-sec}") int clockSkewSec,
-            @Value("${jwt.secret-key}") String secretKey
+            @Value("${jwt.clock-skew-sec}") int clockSkewSec
     ) {
         this.issuer = issuer;
         this.expirationSec = expirationSec;
         this.clockSkewSec = clockSkewSec;
-        this.secretKey = secretKey;
+        this.secretKey = Jwts.SIG.HS256.key().build();
     }
 
     @Override
@@ -49,22 +44,22 @@ public class JWTTokenService implements Clock {
 
     private JwtToken newToken(Map<String, String> attributes, int expirationSec) {
         DateTime now = DateTime.now();
-        Claims claims = Jwts
+        ClaimsBuilder claimsBuilder = Jwts
                 .claims()
-                .setIssuer(issuer)
-                .setIssuedAt(now.toDate());
+                .issuer(issuer)
+                .issuedAt(now.toDate());
 
         if (expirationSec > 0) {
             final DateTime expiresAt = now.plusSeconds(expirationSec);
-            claims.setExpiration(expiresAt.toDate());
+            claimsBuilder.expiration(expiresAt.toDate());
         }
-        claims.putAll(attributes);
+        claimsBuilder.add(attributes);
 
         String token = Jwts
                 .builder()
-                .setClaims(claims)
-                .signWith(HS256, secretKey)
-                .compressWith(COMPRESSION_CODEC)
+                .claims(claimsBuilder.build())
+                .signWith(secretKey)
+                .compressWith(Jwts.ZIP.GZIP)
                 .compact();
 
         return new JwtToken(token);
@@ -74,11 +69,12 @@ public class JWTTokenService implements Clock {
         JwtParser parser = Jwts
                 .parser()
                 .requireIssuer(issuer)
-                .setClock(this)
-                .setAllowedClockSkewSeconds(clockSkewSec)
-                .setSigningKey(secretKey);
+                .clock(this)
+                .clockSkewSeconds(clockSkewSec)
+                .verifyWith(secretKey)
+                .build();
         try {
-            Claims claims = parser.parseClaimsJws(token.getToken()).getBody();
+            Claims claims = parser.parseSignedClaims(token.getToken()).getPayload();
             Map<String, String> body = new HashMap<>();
             for (final Map.Entry<String, Object> e : claims.entrySet()) {
                 body.put(e.getKey(), String.valueOf(e.getValue()));
